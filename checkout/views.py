f3c1035703527067808b677b40ba439db10cc4e9
samagosti7 <sagosti7@gmail.com>
2022-11-21 05:29:27 +0000

    Add media

--- checkout/views.py
+++ checkout/views.py
@@ -141,8 +141,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51M4OsZG4HujvfBDtGO8pcPSRgonGydg3arRxd2zUfIoeTFOFZOWVF14RM59ItMe3fkDTCaDpbmYI8N0FIHGx9P3e00IKqyuQ8q',
-        'client_secret': 'test-client-secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
