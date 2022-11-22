0a77e32cf05351b57e48db36bc02ab9e18683228
samagosti7 <sagosti7@gmail.com>
2022-11-22 21:50:03 +0000

    fix settings.py

--- printables/settings.py
+++ printables/settings.py
@@ -122,11 +122,11 @@ WSGI_APPLICATION = 'printables.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
+ }
 
 
 # Password validation
