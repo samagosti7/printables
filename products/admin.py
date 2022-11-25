d27e92c86f53da31a189b2da955cf7660f5fe9df
samagosti7 <sagosti7@gmail.com>
2022-11-25 04:14:23 +0000

    skeleton complaint url

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, ProductComplaint
 
 # Register your models here.
 
@@ -24,5 +24,14 @@ class CategoryAdmin(admin.ModelAdmin):
     )
 
 
+class ProductComplaintAdmin(admin.ModelAdmin):
+    list_display = (
+        'product',
+        'user',
+        'content'
+    )
+
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(ProductComplaint, ProductComplaintAdmin)
--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -31,3 +32,11 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ProductComplaint(models.Model):
+    product = models.ForeignKey(Product, related_name="complaints", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name="complaints", on_delete=models.CASCADE)
+
+    content = models.TextField(blank=True, null=True)
+    
