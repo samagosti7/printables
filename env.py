79d40b18963e2c0f36e80560fb6c3756c2b93015
samagosti7 <sagosti7@gmail.com>
2023-01-02 20:00:43 +0000

    Add correct database url to env.py

--- env.py
+++ env.py
@@ -1,4 +1,5 @@
 import os
 os.environ['SECRET_KEY'] = '_=*i9y2cmakqz_ccy65&^i-+m4@jtpjmcox&fd@fzx50zropd8'
 os.environ['DEVELOPMENT'] = '1'
-os.environ['DATABASE_URL'] = 'postgres://rgskjddvthoivj:0246dd9f5371fecafe76fb67d4ff4eb3931e90b4bae9e7017bc6e9e21a90e5d0@ec2-52-212-228-71.eu-west-1.compute.amazonaws.com:5432/dd3a8iha4v1o7a'
+# os.environ['DATABASE_URL'] = 'postgres://rgskjddvthoivj:0246dd9f5371fecafe76fb67d4ff4eb3931e90b4bae9e7017bc6e9e21a90e5d0@ec2-52-212-228-71.eu-west-1.compute.amazonaws.com:5432/dd3a8iha4v1o7a'
+os.environ['DATABASE_URL'] = 'postgres://ypacntjr:Cd2UzSsIAe5b19l3XqrACAYl_LlgL747@mouse.db.elephantsql.com/ypacntjr'
