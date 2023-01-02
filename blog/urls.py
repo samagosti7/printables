942ae2b78b1630da0c8be04731259814b3d25d88
samagosti7 <sagosti7@gmail.com>
2023-01-02 20:42:44 +0000

    blog urls

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.blog, name='blog'),
     path('add_post', views.add_post, name='add_post'),
     path('delete_post/<slug:post>/', views.delete_post, name='delete_post'),
-    path('edit_post/<slug:post>/', views.remove_from_bag, name='edit_post'),
+    path('edit_post/<slug:post>/', views.edit_post, name='edit_post'),
     path('<slug:post>/', views.post_detail, name='post_detail'),
 
 ]
