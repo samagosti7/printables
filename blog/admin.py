385db3ca0f94f72e1bed0a33da030e0977e28fe6
samagosti7 <sagosti7@gmail.com>
2023-01-03 14:55:46 +0000

    Create blog template, register admin panel for blog with relevant fields, edit main nav to include blog link on contact dropdown

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+
+class BlogAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'author', 'title', 'slug', 'created_on', 'description', 'status',
+    )
+
+    search_fields = (
+        'author', 'title', 'slug', 'created_on', 'description', 'status',
+    )
+
+
+admin.site.register(Post, BlogAdmin)
