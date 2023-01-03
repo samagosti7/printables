99efa5232ff6e2c3ed023f05b0f721fe4e5f93b0
samagosti7 <sagosti7@gmail.com>
2023-01-03 02:11:21 +0000

    Add blog post view for delete post, and skeleton of same for add post

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('', views.blog, name='blog'),
     path('add_post', views.add_post, name='add_post'),
     path('delete_post/<slug:post>/', views.delete_post, name='delete_post'),
-    path('edit_post/<slug:post>/', views.edit_post, name='edit_post'),
     path('<slug:post>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Post
+from .forms import NewPostForm
+from django.contrib import messages
 
 # Create your views here.
+
+
+@login_required
+def delete_post(request, Post):
+    """
+        Delete Blog Post
+    """
+    if not request.user.is_superuser:
+        messages.error(request, "You are not authorized to perform that action.")
+        return redirect(reverse("blog"))
+
+        post = get_object_or_404(Post, slug=post)
+        post.delete()
+        messages.success("Post deleted.")
+
+
+@login_required
+def delete_post(request, Post):
+    """
+        Delete Blog Post
+    """
+    if not request.user.is_superuser:
+        messages.error(request, "You are not authorized to perform that action.")
+        return redirect(reverse("blog"))
+
+        post = get_object_or_404(Post, slug=post)
+        post.delete()
+        messages.success("Post deleted.")
+
+
+
