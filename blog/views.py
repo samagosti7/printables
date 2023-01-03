f4f146a62ec6e7b3171082e08dd9f412dba8b915
samagosti7 <sagosti7@gmail.com>
2023-01-03 02:27:05 +0000

    create add post view

--- blog/views.py
+++ blog/views.py
@@ -7,21 +7,32 @@ from django.contrib import messages
 # Create your views here.
 
 
-@login_required
-def delete_post(request, Post):
+@login_required()
+def add_post(request):
     """
-        Delete Blog Post
+        Add Blog Post
     """
     if not request.user.is_superuser:
         messages.error(request, "You are not authorized to perform that action.")
         return redirect(reverse("blog"))
 
-        post = get_object_or_404(Post, slug=post)
-        post.delete()
-        messages.success("Post deleted.")
+        if request.method == "POST":
+            form = NewPostForm(request.POST, request.FILES)
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Post submitted")
+                return redirect(reverse("blog"))
+        else:
+            form = NewPostForm()
+        
+        context = {
+            "form": form,
+        }
+    
+    return render(request, 'blog/add_post', context)
 
 
-@login_required
+@login_required()
 def delete_post(request, Post):
     """
         Delete Blog Post
