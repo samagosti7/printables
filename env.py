62a3ee6f0e06bb8ae774e65f11ae6232944b72a9
samagosti7 <sagosti7@gmail.com>
2023-01-04 07:46:25 +0000

    test debug=False to fix bug

--- env.py
+++ env.py
@@ -1,4 +1,4 @@
 import os
 os.environ['SECRET_KEY'] = '_=*i9y2cmakqz_ccy65&^i-+m4@jtpjmcox&fd@fzx50zropd8'
-os.environ['DEVELOPMENT'] = '1'
+# os.environ['DEVELOPMENT'] = '1'
 os.environ['DATABASE_URL'] = 'postgres://ypacntjr:Cd2UzSsIAe5b19l3XqrACAYl_LlgL747@mouse.db.elephantsql.com/ypacntjr'
--- printables/settings.py
+++ printables/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['printables.herokuapp.com', 'localhost']
